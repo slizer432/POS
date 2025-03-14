5be2e814758cc0d15130c8ba2273502a761d69d4
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2025-03-14 09:15:27 +0700

    jobsheet 3 praktikum 4

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'home']);
-
-Route::prefix('/category')->group(function () {
-    Route::get('/food-beverage', [ProductsController::class, 'foodBeverage']);
-    Route::get('/beauty-health', [ProductsController::class, 'beautyHealth']);
-    Route::get('/home-care', [ProductsController::class, 'homeCare']);
-    Route::get('/baby-kid', [ProductsController::class, 'babyKid']);
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/user/{id}/name/{name}', [UserController::class, 'user']);
-
-Route::get('/penjualan', [PenjualanController::class, 'penjualan']);
+Route::get('/level', [LevelController::class, 'index']);
