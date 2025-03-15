77cb69b3764158bf36211826869913e60391c05f
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2025-03-15 16:36:23 +0700

    jobsheet 4 praktikum 2.7

---
+++ app/Models/LevelModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class LevelModel extends Model
+{
+    use HasFactory;
+    protected $table = 'm_level'; // nama table
+    protected $primaryKey = 'level_id'; // primary key pada table tsb
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(UserModel::class);
+    }
+}
